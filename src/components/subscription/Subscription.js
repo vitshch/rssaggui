a266907d930e0c8d701fa36b2c8e630b2d8b664f
vitalish <1n9e9o0!>
2019-07-10 23:16:54 +0300

    initial commit for rssagg ui

---
+++ src/components/subscription/Subscription.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Subscription extends React.Component {
+
+    render() {
+       return (<div>here will be subscriptions</div>);
+    }
+}
+
+export default Subscription;
